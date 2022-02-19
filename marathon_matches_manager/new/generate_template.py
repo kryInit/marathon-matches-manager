50e6bbf1c5eb1f905004ed6bfeafa688a6b647f7
kryInit <kryinit@gmail.com>
2022-02-19 21:02:09 +0900

    get all contest and adjusting

--- marathon_matches_manager/new/generate_template.py
+++ marathon_matches_manager/new/generate_template.py
@@ -2,8 +2,9 @@ import dataclasses
 import logging
 import os
 import re
+import sys
 from collections import OrderedDict
-from typing import List
+from typing import List, Optional
 
 import Levenshtein
 import requests
@@ -46,24 +47,17 @@ def generate_template(name: str) -> None:
 
     os.makedirs(name)
 
-    atcoder_contests = _get_all_atcoder_marathon_contests()
-    predicted_contests = _predict_related_contests(name, atcoder_contests)
+    atcoder_contests = _sorted_contests_by_relevance(name, _get_all_atcoder_marathon_contests())
 
-    print()
-    skipped = False
-    for_this_contest = None
-    for contest in predicted_contests:
-        formatted_contest_name = text_styling(contest.name, color=Fore.CYAN, style=Style.BRIGHT)
-        replay = input(f"this template will be used for {formatted_contest_name}?  [y/n/s(skip)]: ").strip()
-        if replay == "s" or replay == "skip":
-            skipped = True
-            break
-        elif replay == "y" or replay == "yes":
-            for_this_contest = contest
-            break
-    if not skipped and for_this_contest is None:
-        logger.warning("No further contest information available...")
-    print()
+    related_contest = _ask_related_contest(atcoder_contests)
+
+    if related_contest is not None:
+        logger.info(
+            "this project is now associated with following contest\n"
+            "=====================================\n"
+            f"{related_contest}\n"
+            "=====================================\n"
+        )
 
     logger.info(text_styling("CREATION SUCCESSFUL!", color=Fore.GREEN, style=Style.BRIGHT))
 
@@ -74,40 +68,48 @@ def _get_all_atcoder_marathon_contests(overseas: bool = False) -> List[ContestIn
 
     contests = []
 
-    url = CONST.ATCODER_PAST_MARATHON_CONTESTS_URL + "&lang=" + ("en" if overseas else "ja")
-    content = requests.get(url)
-    soup = BeautifulSoup(content.text, "html.parser")
-    tables = soup.find_all("tbody")
-
     detected_contest_count = 0
     recognized_contest_count = 0
 
-    for table in tables:
-        for contest in table.find_all("tr"):
-            detected_contest_count += 1
-            raw_contest_info = contest.find_all("td")
-            if len(raw_contest_info) == 4:
-                raw_contest_info[0] = raw_contest_info[0].find("time")
-                raw_contest_info[1] = raw_contest_info[1].find("a")
+    page_idx = 0
+    while True:
+        page_idx += 1
+        url = f"{CONST.ATCODER_PAST_MARATHON_CONTESTS_URL}&lang={'en' if overseas else 'ja'}&page={page_idx}"
+        content = requests.get(url)
+        soup = BeautifulSoup(content.text, "html.parser")
+        tables = soup.find_all("tbody")
 
-                if all(raw_contest_info):
-                    start_time, contest_name, period, rated = map(lambda x: x.text, raw_contest_info)
+        if not tables or page_idx >= 30:
+            break
 
-                    sub_contest_name = raw_contest_info[1].get("href")
-                    sub_contest_name = "" if sub_contest_name is None else sub_contest_name.lstrip("/contests/")
+        for table in tables:
+            for contest in table.find_all("tr"):
+                detected_contest_count += 1
+                raw_contest_info = contest.find_all("td")
+                if len(raw_contest_info) == 4:
+                    raw_contest_info[0] = raw_contest_info[0].find("time")
+                    raw_contest_info[1] = raw_contest_info[1].find("a")
 
-                    recognized_contest_count += 1
-                    contests.append(
-                        ContestInfo(
-                            name=contest_name,
-                            sub_name=sub_contest_name,
-                            start_time=start_time,
-                            period=period,
-                            rated=rated,
+                    if all(raw_contest_info):
+                        start_time, contest_name, period, rated = map(lambda x: x.text, raw_contest_info)
+
+                        sub_contest_name = raw_contest_info[1].get("href")
+                        sub_contest_name = "" if sub_contest_name is None else sub_contest_name.lstrip("/contests/")
+
+                        recognized_contest_count += 1
+                        contests.append(
+                            ContestInfo(
+                                name=contest_name,
+                                sub_name=sub_contest_name,
+                                start_time=start_time,
+                                period=period,
+                                rated=rated,
+                            )
                         )
-                    )
 
-    logger.debug(f"scraping contests. recognized / detected: {recognized_contest_count} / {detected_contest_count}")
+    logger.info(
+        f"contests scraping is completed. recognized / detected: {recognized_contest_count} / {detected_contest_count}"
+    )
 
     return contests
 
@@ -125,12 +127,9 @@ def _calc_edit_distance(s1: str, s2: str) -> float:
     return (d1 + d2) / 2
 
 
-def _predict_related_contests(raw_name: str, atcoder_contests: List[ContestInfo]) -> List[ContestInfo]:
-    # name, url, period, in progress,
-
+def _sorted_contests_by_relevance(raw_name: str, atcoder_contests: List[ContestInfo]) -> List[ContestInfo]:
     name = _normalize_contest_name(raw_name)
-
-    edit_dists = list(
+    return list(
         map(
             lambda x: atcoder_contests[x],
             OrderedDict.fromkeys(
@@ -149,4 +148,35 @@ def _predict_related_contests(raw_name: str, atcoder_contests: List[ContestInfo]
         )
     )
 
-    return edit_dists
+
+def _ask_related_contest(atcoder_contests: List[ContestInfo]) -> Optional[ContestInfo]:
+    logger = logging.getLogger(__name__)
+    n = len(atcoder_contests)
+    ndn = len(str(n))
+
+    print()
+    skipped = False
+    for_this_contest = None
+    for th, contest in enumerate(atcoder_contests):
+        progress = f"[{th+1:{ndn}d}/{n}] " if th != 0 else ""
+        formatted_contest_name = text_styling(contest.name, color=Fore.CYAN, style=Style.BRIGHT)
+
+        replay = input(f"{progress}this template will be used for {formatted_contest_name}?  [y/n/s(skip)]: ").strip()
+
+        # yes: ye[s]
+        if replay.endswith(("y", "yes")):
+            for_this_contest = contest
+            break
+        elif replay.endswith(("s", "skip")):
+            skipped = True
+            break
+
+        # todo: adjust to terminal size
+        print("\033[1A\033[2K", end="")
+        sys.stdout.flush()
+
+    if not skipped and for_this_contest is None:
+        logger.info("No further contest information available...")
+    print()
+
+    return for_this_contest
