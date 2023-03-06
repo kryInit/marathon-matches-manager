256471a2c9e787b692a323bfd229725292a42919
kotaro sakata <kotarosakata2002@gmail.com>
2023-03-06 12:12:49 +0900

    chenge init command

--- marathon_matches_manager/lib/new/generate_template.py
+++ marathon_matches_manager/lib/new/generate_template.py
@@ -8,25 +8,19 @@ from ..utils import text_styling
 from .ask_relate_contest import ask_related_contest
 
 
-def generate_template(name: str,exists: bool) -> None:
+def generate_template(name: str,has_create: bool) -> None:
     styled_name = text_styling(name, color=Fore.CYAN, style=Style.BRIGHT)
     logger = logging.getLogger(__name__)
     logger.info(f"start generating template as '{styled_name}'")
-
-    path = Path.cwd().joinpath(name)
-
-    if Path.exists(path) and not exists:
-        logger.error(f"a directory named '{styled_name}' is already exists.")
-        return
-
-    if not Path.exists(path) and exists:
-        logger.error(f"a directory named '{styled_name}' is not exists.")
-        return
-    
-    if not exists:
+    if not has_create:
+        path = Path.cwd().joinpath(name)
+        if path.exists():
+            logger.error(f"a directory named '{styled_name}' is already exists.")
+            return        
         path.mkdir()
+        path.joinpath("m3-config.toml").touch()
+
     # path.joinpath(".m3").mkdir()
-    path.joinpath("m3-config.toml").touch()
 
     try:
         related_contest = ask_related_contest(name)
